ef79404178244dcf74188dc9730523cb2dbf1a23
CodyAdam <cody.ad4@gmail.com>
2023-03-08 11:11:59 +0100

    feat: add socket hook

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import Canvas from '@/components/Canvas';
+import useSocket from '@/hooks/useSocket';
+
+const SOCKET_URL = 'http://localhost:8080';
+
 export default function Home() {
+    const socket = useSocket(SOCKET_URL);
     return (
-        <main>Cody le troll.fr</main>
+        <main>
+            <div>Is connected : {JSON.stringify(socket?.connected)}</div>
+            <Canvas />
+        </main>
     )
 }
---
+++ apps/web/components/Canvas.tsx
@@ -0,0 +1,5 @@
+export default function Canvas(props: {}) {
+  return <canvas>
+    
+  </canvas>
+}
