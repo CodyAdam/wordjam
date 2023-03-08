90d79481717989da31d0a824d7e09bd291a53d8a
CodyAdam <cody.ad4@gmail.com>
2023-03-08 11:14:13 +0100

    bug: not working comment

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import Canvas from '@/components/Canvas';
-import useSocket from '@/hooks/useSocket';
+// import useSocket from '@/hooks/useSocket';
 
 const SOCKET_URL = 'http://localhost:8080';
 
 export default function Home() {
-    const socket = useSocket(SOCKET_URL);
+    // const socket = useSocket(SOCKET_URL);
     return (
         <main>
-            <div>Is connected : {JSON.stringify(socket?.connected)}</div>
+            {/* <div>Is connected : {JSON.stringify(socket?.connected)}</div> */}
             <Canvas />
         </main>
     )
