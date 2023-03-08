01cb6f1022b2539a793beb2b523b0c99e085de13
fgdou <fabigoardou@gmail.com>
2023-03-08 12:07:39 +0100

    feat: starting dictionnary tests

---
+++ apps/server/Disctionnary.test.ts
@@ -0,0 +1,10 @@
+import {DictionnaryService} from "./Dictionnary";
+import {describe, it} from "node:test";
+
+describe("DictionnaryService", ()=>{
+    describe("Test words", () => {
+        it('Words not exists', () => {
+            describe("")
+        })
+    })
+})
