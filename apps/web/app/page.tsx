2e8a5f38bcd21cbaa3e576d28c2fb4c61aa41bef
Pixselve <38262536+Pixselve@users.noreply.github.com>
2023-03-29 10:54:08 +0200

    feat: modal

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,14 @@
-'use client';
-import Board from '@/src/components/Board';
-import { useSocket } from '@/src/hooks/useSocket';
-import { BoardLetter } from '@/src/types/board';
-import { useEffect, useState } from 'react';
-import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
-import { AppState } from '@/src/lib/AppState';
-import Login from '@/src/components/Login';
-import { SOCKET_URL } from '@/src/lib/config';
+"use client";
+import Board from "@/src/components/Board";
+import { useSocket } from "@/src/hooks/useSocket";
+import { BoardLetter } from "@/src/types/board";
+import { useState } from "react";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { AppState } from "@/src/lib/AppState";
+import Login from "@/src/components/Login";
+import { SOCKET_URL } from "@/src/lib/config";
 import LinkDeviceButton from "@/src/components/LinkDeviceButton";
+import TokenModal from "@/src/components/TokenModal";
 
 export default function App() {
   const [placedLetters, setPlacedLetters] = useState<BoardLetter[]>([]);
@@ -28,7 +29,6 @@ export default function App() {
 
   const [showTokenModal, setShowTokenModal] = useState(false);
 
-
   function onLogin(token: string) {
     if (!token) {
       token = localStorage.getItem("token") || "";
@@ -40,41 +40,33 @@ export default function App() {
   }
 
   return (
-    <main className='bg-grid flex h-full items-center justify-center bg-white relative'>
+    <main className="bg-grid relative flex h-full items-center justify-center bg-white">
       <div className="[&>div]:h-screen [&>div]:w-screen ">
         <TransformWrapper centerOnInit initialScale={3}>
           <TransformComponent>
             <Board placedLetters={placedLetters} />
           </TransformComponent>
         </TransformWrapper>
-
-
-
       </div>
 
       {showTokenModal && (
-        <div className="absolute p-2 bg-black/20 backdrop-blur-sm w-full h-full z-20 flex justify-center items-center">
-          <div className="rounded-lg bg-white p-10 shadow space-y-4">
-            <div className="text-xl font-bold">Your token</div>
-            <input disabled value={playerToken} className='focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none'></input>
-            <button className='w-full rounded-md bg-green-400 p-2 text-xl font-bold text-white transition-colors hover:bg-green-500'>
-              Done
-            </button>
-
-          </div>
-
-        </div>
+        <TokenModal
+          value={playerToken}
+          onClick={() => setShowTokenModal(false)}
+        />
       )}
-
 
       {appStage === AppState.InGame && (
         <div className="absolute top-0 right-0 p-2">
-          <LinkDeviceButton></LinkDeviceButton>
+          <LinkDeviceButton
+            onClick={() => setShowTokenModal(true)}
+          ></LinkDeviceButton>
         </div>
       )}
 
-      {appStage === AppState.AwaitingLogin && <Login onLogin={onLogin} socket={socket} isConnected={isConnected} />}
-
+      {appStage === AppState.AwaitingLogin && (
+        <Login onLogin={onLogin} socket={socket} isConnected={isConnected} />
+      )}
     </main>
   );
 }
--- apps/web/src/components/LinkDeviceButton.tsx
+++ apps/web/src/components/LinkDeviceButton.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 
-export default function LinkDeviceButton() {
+type LinkDeviceButtonProps = {
+  onClick: () => void;
+}
+
+export default function LinkDeviceButton({ onClick }: LinkDeviceButtonProps) {
   return (
     <>
-    <button className='w-full rounded-md bg-green-400 p-2 text-xl font-bold text-white transition-colors hover:bg-green-500 flex gap-2 items-center'>
+    <button onClick={onClick} className='w-full rounded-md bg-green-400 p-2 text-xl font-bold text-white transition-colors hover:bg-green-500 flex gap-2 items-center'>
       <Image height={20} width={20} src="/smartphone.png" alt="phone"></Image>
       <span>Link a device</span>
       </button>
