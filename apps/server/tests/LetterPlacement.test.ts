5f0b88a08ef499f19362b9ffcbb9f7a99e4bf013
CodyAdam <cody.ad4@gmail.com>
2023-03-29 14:27:45 +0200

    fix: the test with the new func

--- apps/server/tests/LetterPlacement.test.ts
+++ apps/server/tests/LetterPlacement.test.ts
@@ -1,17 +1,56 @@
-import {beforeEach, describe} from "node:test";
-import {PlacedResponse, PlaceWord, Direction} from "../src/types/board";
-import {checkLetterPlacedFromClient} from "../src/server";
+import { beforeEach, describe } from 'node:test';
+import { PlacedResponse, PlaceWord, Direction } from '../src/types/board';
+import { checkLetterPlacedFromClient } from '../src/server';
+import { Player } from '../src/types/player';
 
-describe("LetterPlacement", () => {
-    beforeEach(() => {
-        initServerPlaceWordsTest();
-    });
+describe('LetterPlacement', () => {
+  beforeEach(() => {
+    initServerPlaceWordsTest();
+  });
 
-    test("placeWords", () => {
-        expect(PlacedResponse.OK).toEqual(checkLetterPlacedFromClient({letters: [], startPos: {x:0, y:0}, direction: Direction.DOWN}, "test"))
-    });
+  test('placeWords', () => {
+    const oneHourAgo = new Date();
+    oneHourAgo.setHours(oneHourAgo.getHours() - 1);
+
+    const player: Player = {
+      username: 'tester',
+      token: 'test',
+      cooldownTarget: oneHourAgo,
+      score: 0,
+      letters: [
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+      ],
+    };
+    if (!player) throw new Error('Player not found');
+    expect(PlacedResponse.OK).toEqual(
+      checkLetterPlacedFromClient({ letters: [], startPos: { x: 0, y: 0 }, direction: Direction.DOWN }, player),
+    );
+  });
 });
 
-function initServerPlaceWordsTest(){
-
-}
+function initServerPlaceWordsTest() {}
