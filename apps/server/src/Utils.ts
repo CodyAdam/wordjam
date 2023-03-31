839b8e4b69da2c27fa2392b6c5b57743dcdf68c4
yami2200 <TheYami22@gmail.com>
2023-03-31 18:20:43 +0200

    clean Utils functions. add comments.

--- apps/server/src/Utils.ts
+++ apps/server/src/Utils.ts
@@ -1,20 +1,34 @@
 import {Config} from "./config";
 import {DictionaryService} from "./Dictionary";
 
+/**
+ * get a date that represent the date from now when the cooldown will be over
+ * @returns the date when the cooldown will be over
+ */
 export function getDatePlusCooldown() {
     return new Date(new Date().getTime() + Config.LETTER_COOLDOWN * 1000);
 }
 
+/**
+ * generate a random token of a given length with letters and numbers
+ * @param len the length of the token
+ * @returns the generated token
+ */
 export function generateToken(len: number): string {
-    let text = '';
-    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < len; i++) text += charset.charAt(Math.floor(Math.random() * charset.length));
+    let text : string = '';
+    const charset : string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i : number = 0; i < len; i++) text += charset.charAt(Math.floor(Math.random() * charset.length));
 
     return text;
 }
 
-export function generateLetters(number: number) {
+/**
+ * generate an array with given length of random letters
+ * @param len the length of the array
+ * @returns the array of random letters
+ */
+export function generateLetters(len: number) {
     let letters = [];
-    for (let i = 0; i < number; i++) letters.push(DictionaryService.getRandomLetter());
+    for (let i = 0; i < len; i++) letters.push(DictionaryService.getRandomLetter());
     return letters;
 }
