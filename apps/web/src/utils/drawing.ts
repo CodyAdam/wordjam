5aa4f30f0debfb3dcc19f872107d04ee3f92f66b
CodyAdam <cody.ad4@gmail.com>
2023-04-05 20:30:47 +0200

    feat: inventory colors

--- apps/web/src/utils/drawing.ts
+++ apps/web/src/utils/drawing.ts
@@ -48,14 +48,13 @@ export function drawDebug(ctx: CanvasRenderingContext2D, pos: Position, text: st
 export function drawPlacedLetters(ctx: CanvasRenderingContext2D, placedLetters: BoardLetters, pan: Pan) {
   placedLetters.forEach((letter) => {
     const pos = worldToScreen(posCentered(letter.position), pan);
-    ctx.fillStyle = 'black';
+
     // use scaled font size
     const fontSize = pan.scale * TILE_SIZE * 0.035;
     ctx.font = `${fontSize}px ${boardFont.style.fontFamily}`;
     ctx.fillStyle = 'rgb(63 63 70)';
 
-    // offset by .5 of the letter width to center it same for height
-
+// offset by .5 of the letter width to center it same for height
     const letterOffset = { x: ctx.measureText(letter.letter).width / 2, y: fontSize / 2.9 };
     ctx.fillText(letter.letter, pos.x - letterOffset.x, pos.y + letterOffset.y);
   });
@@ -65,12 +64,16 @@ export function drawPlacedInventoryLetters(ctx: CanvasRenderingContext2D, placed
   placedLetters.forEach((letter) => {
     if (!letter.position) return;
     const pos = worldToScreen(posCentered(letter.position), pan);
-    ctx.fillStyle = 'black';
+
     // use scaled font size
-    const fontSize = pan.scale * TILE_SIZE * 0.025;
-    ctx.font = `${fontSize}px Arial`;
-    const letterOffset = pan.scale * TILE_SIZE * 0.01;
-    ctx.fillText(letter.letter, pos.x - letterOffset, pos.y + letterOffset);
+    const fontSize = pan.scale * TILE_SIZE * 0.035;
+    ctx.font = `${fontSize}px ${boardFont.style.fontFamily}`;
+    ctx.fillStyle = 'rgb(63 63 70  / 0.3)';
+
+    
+    // offset by .5 of the letter width to center it same for height
+    const letterOffset = { x: ctx.measureText(letter.letter).width / 2, y: fontSize / 2.9 };
+    ctx.fillText(letter.letter, pos.x - letterOffset.x, pos.y + letterOffset.y);
   });
 }
 
