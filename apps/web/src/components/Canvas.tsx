b646c85bd4f6d898ce7bde109136e18133f4da9e
CodyAdam <cody.ad4@gmail.com>
2023-04-05 18:27:22 +0200

    fix: touch errors

--- apps/web/src/components/Canvas.tsx
+++ apps/web/src/components/Canvas.tsx
@@ -54,12 +54,13 @@ export default function Canvas({
   const onDown = useCallback((e: React.TouchEvent | React.MouseEvent) => {
     let x = 0;
     let y = 0;
-    e.preventDefault();
     e.stopPropagation();
     if (e.nativeEvent instanceof TouchEvent) {
+      if (e.nativeEvent.touches.length === 0) return;
       x = e.nativeEvent.touches[0].clientX;
       y = e.nativeEvent.touches[0].clientY;
     } else if (e.nativeEvent instanceof MouseEvent) {
+      e.preventDefault();
       x = e.nativeEvent.clientX;
       y = e.nativeEvent.clientY;
     } else {
@@ -73,12 +74,13 @@ export default function Canvas({
     (e: React.TouchEvent | React.MouseEvent) => {
       let x = 0;
       let y = 0;
-      e.preventDefault();
       e.stopPropagation();
       if (e.nativeEvent instanceof TouchEvent) {
+        if (e.nativeEvent.touches.length === 0) return;
         x = e.nativeEvent.touches[0].clientX;
         y = e.nativeEvent.touches[0].clientY;
       } else if (e.nativeEvent instanceof MouseEvent) {
+        e.preventDefault();
         x = e.nativeEvent.clientX;
         y = e.nativeEvent.clientY;
       } else {
@@ -102,14 +104,16 @@ export default function Canvas({
 
   const onMove = useCallback(
     (e: React.TouchEvent | React.MouseEvent) => {
+      console.log(e);
       let x = 0;
       let y = 0;
-      e.preventDefault();
       e.stopPropagation();
       if (e.nativeEvent instanceof TouchEvent) {
+        if (e.nativeEvent.touches.length === 0) return;
         x = e.nativeEvent.touches[0].clientX;
         y = e.nativeEvent.touches[0].clientY;
       } else if (e.nativeEvent instanceof MouseEvent) {
+        e.preventDefault();
         x = e.nativeEvent.clientX;
         y = e.nativeEvent.clientY;
       } else {
