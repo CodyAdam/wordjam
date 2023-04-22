b42f4134b4e93adb7eebf44d18dc5317e40bd063
CodyAdam <cody.ad4@gmail.com>
2023-04-22 12:55:46 +0200

    feat: change highlight colors

--- apps/server/src/Config.ts
+++ apps/server/src/Config.ts
@@ -1,6 +1,6 @@
 export class Config {
     static LETTER_COOLDOWN = 5*60;
-    static COLOR_HIGHLIGHT_ERROR = "#ff0000";
-    static COLOR_HIGHLIGHT_VALID = "#00ff00";
+    static COLOR_HIGHLIGHT_ERROR = "#ef4444";
+    static COLOR_HIGHLIGHT_VALID = "#22c55e";
     static MIN_HAND_LETTERS = 7;
 }
