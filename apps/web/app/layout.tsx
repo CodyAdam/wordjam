25e11e0cb214163cce191c98548030e1c366e2a4
CodyAdam <cody.ad4@gmail.com>
2023-04-22 16:41:01 +0200

    fix: viewport zoom

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,12 +4,17 @@ import { uiFont } from '@/src/utils/fontLoader';
 export const metadata = {
   title: 'Wordjam',
   description: 'A multiplayer word game',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: 0,
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className={`h-full ${uiFont.className}`}>
-      <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'></meta>
       <body className='relative flex h-full items-center justify-center overflow-hidden'>{children}</body>
     </html>
   );
