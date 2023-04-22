03b37481aa962dd318ebb4f998ea0244d2decc2a
CodyAdam <cody.ad4@gmail.com>
2023-04-22 12:41:13 +0200

    feat: better spinner

--- apps/web/src/components/Login.tsx
+++ apps/web/src/components/Login.tsx
@@ -6,6 +6,8 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { Socket } from 'socket.io-client';
 import { LoginResponseType } from '../types/api';
 import MaterialSymbolsFramePersonSharp from './svg/MaterialSymbolsFramePersonSharp';
+import SvgSpinnersGooeyBalls1 from './svg/SvgSpinnersGooeyBalls1';
+import SvgSpinnersBlocksShuffle3 from './svg/SvgSpinnersBlocksShuffle3';
 
 enum Type {
   Nickname,
@@ -54,21 +56,9 @@ export default function Login({ isConnected, socket }: { isConnected: boolean; s
           className='flex flex-col gap-3 rounded-2xl border-b-8 border-gray-200 bg-white p-10 shadow-xl'
         >
           {HEADER}
-          <div className='flex flex-col items-center justify-center gap-4 pt-6'>
-            <svg
-              className='-ml-1 mr-3 h-10 w-10 animate-spin text-red-500'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-            >
-              <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
-              <path
-                className='opacity-75'
-                fill='currentColor'
-                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-              ></path>
-            </svg>
-            <h1 className='text-2xl font-bold text-gray-700'>Connecting to server...</h1>
+          <div className='flex flex-col items-center justify-center gap-4 pt-6 text-gray-700 animate-pulse'>
+            <h1 className='text-lg font-semibold '>Connecting to the server...</h1>
+            <SvgSpinnersBlocksShuffle3 className='w-10 h-10' />
           </div>
         </form>
       </div>
