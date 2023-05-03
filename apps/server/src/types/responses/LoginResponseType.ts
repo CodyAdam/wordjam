4e7c0626dced42fca078f8ba6e6513b18dc3932d
fgdou <fabigoardou@gmail.com>
2023-05-03 11:24:59 +0200

    feat: trim username

--- apps/server/src/types/responses/LoginResponseType.ts
+++ apps/server/src/types/responses/LoginResponseType.ts
@@ -2,5 +2,6 @@ export enum LoginResponseType {
     SUCCESS = "SUCCESS",
     WRONG_TOKEN = "WRONG_TOKEN",
     ALREADY_EXIST = "ALREADY_EXIST",
-    TOO_LONG = "TOO_LONG"
+    TOO_LONG = "TOO_LONG",
+    EMPTY = "EMPTY"
 }
