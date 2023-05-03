884d365e1d4be5afe0a4504729bed26261e59850
CodyAdam <cody.ad4@gmail.com>
2023-05-03 11:12:43 +0200

    fix: max length front

--- apps/web/src/components/Login.tsx
+++ apps/web/src/components/Login.tsx
@@ -75,6 +75,7 @@ export default function Login({ isConnected, socket }: { isConnected: boolean; s
           {HEADER}
           <input
             className='focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 focus:outline-none'
+            maxLength={20}
             type='text'
             placeholder='Nickname'
             {...register('nicknameOrToken', { required: true })}
