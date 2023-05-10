dbc839bf02be59204bada43a1e9f299958266841
Pixselve <38262536+Pixselve@users.noreply.github.com>
2023-05-10 11:15:17 +0200

    feat: url for scoreboard

--- apps/web/app/leaderboard/page.tsx
+++ apps/web/app/leaderboard/page.tsx
@@ -1,36 +1,27 @@
-export default function LeaderboardPage() {
+export default async function LeaderboardPage() {
 
-    const dummyData = new Array(200).fill(0).map((_, i) => {
-        return {
-            rank: i + 1,
-            name: `Player ${i + 1}`,
-            score: 100,
-            founder: Math.random() > 0.5,
-        }
-
-    })
+    const dummyData: {username: string, score: number}[] = await fetch("https://wordjam.fgdou.ovh/api/leaderboard?count=100").then(r => r.json());
 
 
     return <div className="grow h-full p-10 bg-white rounded-xl shadow overflow-y-scroll">
-        <table className="w-full">
+        <table className="w-full ">
             <thead>
-                <tr>
-                    <th className="text-left p-2">Rank</th>
-                    <th className="text-left p-2">Player</th>
-                    <th className="text-left p-2">Score</th>
-                </tr>
+            <tr>
+                <th className="text-left p-2">Rank</th>
+                <th className="text-left p-2">Player</th>
+                <th className="text-left p-2">Score</th>
+            </tr>
             </thead>
             <tbody>
             {dummyData.map((d, i) => (
-                    <tr key={d.name} className="border-b-2 border-t-2 border-white">
-                        <td className="bg-gray-100 p-2">#{d.rank}</td>
-                        <td className="bg-gray-100 p-2">
-                            <span className="uppercase font-bold">{d.name}</span>
-                            {d.founder && <span className="ml-2 bg-red-500 rounded-full text-xs px-2 inline-flex items-center justify-center text-white">Founder 👑</span>}
-                        </td>
-                        <td className="bg-gray-100 p-2">{d.score}</td>
-                    </tr>
-                ))}
+              <tr key={d.username} className="border-b-2 border-t-2 border-white ">
+                  <td className="bg-gray-100 p-2">#{i + 1}</td>
+                  <td className="bg-gray-100 p-2">
+                      <div className="uppercase font-bold truncate overflow-hidden max-w-xs">{d.username}</div>
+                  </td>
+                  <td className="bg-gray-100 p-2">{d.score}</td>
+              </tr>
+            ))}
             </tbody>
         </table>
     </div>
