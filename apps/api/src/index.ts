7cea5f8a1ee3e60d1a4c6f7a2cbbfb6fd2aa8c75
fgdou <fabigoardou@gmail.com>
2023-05-10 11:27:29 +0200

    fix: env on api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
 import Fastify, {FastifyInstance} from 'fastify'
 import {prisma} from "./prisma.js";
 import {Static, Type} from "@sinclair/typebox";
